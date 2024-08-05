42be82cd4f6084b5b21b1a6f8e2ae1a46eb1f80c
Samuel Florez <samuelflorezf@gmail.com>
2024-08-05 14:51:21 -0500

    Proyecto Integrador Etapa 2. Se agregan las cards de manera dinamica, con miras a hacer un template para el catch error

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
+import Handlebars from "handlebars";
 import './sass/main.scss'
+
+const start = async() => {
+
+    try {
+        const respuesta = await fetch('templates/card.hbs')
+
+        if (!respuesta.ok) {
+            throw new Error ('No se pudo obtener la plantillla')
+        }
+        const plantilla = await respuesta.text()
+
+        //console.log(plantilla)
+
+
+        const template = Handlebars.compile(plantilla)
+
+
+        const respuestaBack = await fetch('http://localhost:8080/productos/')
+        if (!respuestaBack.ok){
+            throw new Error("Algo paso con los productos", respuestaBack.status)
+        }
+        const dataProductos = await respuestaBack.json()
+        //console.log(dataProductos)
+
+
+        const data = {productos : dataProductos}
+        //console.log(data)
+        const html = template(data)
+        //console.log(html)
+
+        const contenedorCard = document.querySelector('#contenedor-cards')
+        contenedorCard.innerHTML = html
+
+    } catch (error) {
+        console.log('Hubo un error en la peticion:', error)
+    }
+}
+
+
+
+
+
+window.addEventListener('DOMContentLoaded', start)
